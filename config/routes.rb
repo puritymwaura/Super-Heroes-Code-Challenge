33802f05e1946189018c5ebf054d3bd33c04dcc6
michael kaburu <michael.kaburu@student.moringaschool.com>
2022-10-07 17:52:17 +0300

    create index,show routes on heroescontroller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  resources :hero_powers
-  resources :heros
-  resources :powers
+  resources :hero_powers,only:[:create]
+  resources :heroes,only:[:index,:show]
+  resources :powers,only:[:index,:show,:update]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
